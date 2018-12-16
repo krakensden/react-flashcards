6183c89232d5473eb0945dacfd2573c35691d935
kd <krakensden@krakensden.com>
2018-12-16 10:38:18 -0800

    Set up two modes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+/*
+ * So there are two modes (tabs for v0), "enter flashcard", "practice flashcards".
+ * A flashcard has a question and an answer field
+ * Flashcards are a list of objects in localstorage
+ * Practice goes through the list in order, asking questions. Answering is on the honor system.
+ * Tap once, get a Q. Tap again, get the answer. Tap again, next Q.
+ */
+
+function Cards(props) {
+    return props.cards.map((step, card) => {
+        return (
+            <li key={step}>
+            { card['question'] ? card['question'] : '(blank)' }
+            </li>
+        );
+    }
     );
-  }
+}
+
+function Entry(props) {
+    return (
+        <form>
+        hello world
+        <label for="title">Title:</label><input name="title"></input>
+        <textarea name="question"></textarea>
+        <textarea name="answer"></textarea>
+        <input onClick={props.OnClick}></input>
+        </form>
+    );
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        let cards = [];
+        if (localStorage.hasOwnProperty("flashcards")) {
+            cards = localStorage["flashcards"].slice();
+        }
+        this.state = {
+            mode: null,
+            cards: cards,
+        };
+        this.switchMode();
+    }
+
+    switchMode() {
+        if (this.state.mode === "enter") {
+            this.setState({
+                mode: "review",
+                cards: this.state.cards,
+                contents: <ol>
+                    <Cards cards={this.state.cards}/>
+                </ol>,
+            })
+        } else {
+            this.setState({
+                mode: "enter",
+                cards: this.state.cards,
+                contents: <Entry cards={this.state.cards}/>,
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <nav>
+             <h1>{this.state.mode} <button onClick={()=>{this.switchMode()}}>switch</button></h1>
+            </nav>
+            {this.state.contents}
+            </div>
+        );
+    }
 }
 
 export default App;
